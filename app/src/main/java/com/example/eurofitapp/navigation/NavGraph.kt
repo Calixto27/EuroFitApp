0f76976ef96ad56bcf35697554a78cad9031866c
Calixto <ancalopezromera@gmail.com>
2025-02-28 16:36:47 +0100

    First Commit
    Start the proyect and save in git

---
+++ app/src/main/java/com/example/eurofitapp/navigation/NavGraph.kt
@@ -0,0 +1,21 @@
+package com.example.eurofitapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+sealed class Screens(val route: String){
+    object Login: Screens("login_screen")
+    object Home: Screens("home_screen")
+}
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Screens.Login.route){
+        composable(Screens.Login.route){ LoginScreen(navController)}
+        composable(Screens.Home.route){HomeScreen(navController)}
+    }
+}
